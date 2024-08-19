9de00b031581eba0dee6109e168b8f9237ebd982
iori sato <iori1822@icloud.com>
2024-08-19 18:25:36 +0900

    useContext

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode, createContext } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const authorInfo = {
+  name: "iori",
+  age: 23,
+};
+
+export const authorInfoContext = createContext(authorInfo);
+
+createRoot(document.getElementById("root")!).render(
+  <authorInfoContext.Provider value={authorInfo}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+    ,
+  </authorInfoContext.Provider>,
+);
